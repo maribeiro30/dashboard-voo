658857ef618a3dbbb7fc2ab241cf01f9481bdddf
Marco Antonio Ribeiro <marcoribeiro@saofrancisco.com.br>
2018-12-14 15:35:34 -0200

    exibindo voos

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular/core';
+
+export interface PainelVoo{
+    
+}
+
+@Component({
+    selector: 'dashboard',
+    template: './dashboard.component.html',
+    styleUrls: ['./app.component.css']
+
+})
+export class DashboardComponent {
+    title = 'dashboard-voo';
+}
+
+  
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,2 @@
+//https://valor-software.com/ngx-bootstrap/#/getting-started
+//https://material.angular.io/components/table/examples
